65e0006ef07da2577ca27b31bc988f1a60bf2e14
mooh_oli <morganaoliveira59@gmail.com>
2020-03-10 13:51:29 -0300

    Criação dos Validator

---
+++ BLL/Validators/ClienteValidator.cs
@@ -0,0 +1,23 @@
+﻿using DTO;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Validators
+{
+    internal class ClienteValidator : AbstractValidator<ClienteDTO>
+    {
+        public ClienteValidator()
+        {
+
+            RuleFor(c => c.Nome).NotNull().WithMessage("O nome deve ser informado.");
+
+            RuleFor(c => c.Email).NotNull().WithMessage("O email deve ser informado.");
+
+            RuleFor(c => c.Telefone).NotNull().WithMessage("O telefone deve ser informado.");
+
+            RuleFor(c => c.Cpf).NotNull().WithMessage("O CPF deve ser informado.");
+        }
+    }
+}
--- DeliveryCORE/Controllers/ClienteController.cs
+++ DeliveryCORE/Controllers/ClienteController.cs
@@ -59,4 +59,4 @@ namespace DeliveryCORE.Controllers
 
 
 }
-    }
+    
