31fd8d20c689ac486f3d16ca07b387f6bc7a6a2c
Mayara Stresser <mstresses@hotmail.com>
2020-03-10 13:54:37 -0300

    novo

--- DeliveryCORE/Models/Insert/PedidoInsertViewModel.cs
+++ DeliveryCORE/Models/Insert/PedidoInsertViewModel.cs
@@ -2,6 +2,8 @@
 using DTO.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +11,23 @@ namespace DeliveryCORE.Models.Insert
 {
     public class PedidoInsertViewModel
     {
+        [DisplayName("Restaurante"), Required(ErrorMessage = "O restaurante deve ser informado.")]
         public RestauranteDTO Restaurante { get; set; }
+
+        [DisplayName("Produto"), Required(ErrorMessage = "O produto deve ser informado.")]
         public ProdutoDTO Produto { get; set; }
+
+        [DisplayName("Quantidade"), Required(ErrorMessage = "A quantidade deve ser informada.")]
         public double Quantidade { get; set; }
+
+        [DisplayName("ValorTotal"), ReadOnly(true)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = ("R$0,00"))]
         public double ValorTotal { get; set; }
+
+        [DisplayName("TaxaEntrega"), Required(ErrorMessage = "A taxa de entrega deve ser informada.")]
         public bool TaxaEntrega { get; set; }
+
+        [DisplayName("FormaPagamento"), Required(ErrorMessage = "A forma de pagamento deve ser informada.")]
         public FormaPagamento FormaPagamento { get; set; }
     }
 }
--- DeliveryCORE/Models/Insert/RestauranteInsertViewModel.cs
+++ DeliveryCORE/Models/Insert/RestauranteInsertViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using DTO.ComplexTypes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +10,22 @@ namespace DeliveryCORE.Models
 {
     public class RestauranteInsertViewModel
     {
+        [DisplayName("NomeFantasia"), Required(ErrorMessage = ("O nome deve ser informado."))]
+        [StringLength(maximumLength: 30, MinimumLength = 2, ErrorMessage = "O nome do restaurante deve conter entre 2 e 30 caracteres.")]
         public string NomeFantasia { get; set; }
+
+        [DisplayName("CNPJ"), Required(ErrorMessage = ("O CNPJ deve ser informado."))]
+        [StringLength(maximumLength: 18, MinimumLength = 2, ErrorMessage = "O CNPJ deve conter no máximo 18 caracteres.")]
         public string CNPJ { get; set; }
+
+        [DisplayName("Telefone"), Required(ErrorMessage = "O telefone deve ser informado")]
+        [StringLength(maximumLength: 11, MinimumLength = 10, ErrorMessage = "O telefone deve conter no minimo 10 caracteres")]
         public string Telefone { get; set; }
-        public Endereco Endereco { get; set; }
+
+        //public Endereco Endereco { get; set; }
+
+        [DisplayName("Categoria"), Required(ErrorMessage = ("A categoria deve ser informada."))]
+        [StringLength(maximumLength: 30, MinimumLength = 2, ErrorMessage = "A categoria do restaurante deve conter entre 2 e 30 caracteres.")]
         public string Categoria { get; set; }
     }
 }
