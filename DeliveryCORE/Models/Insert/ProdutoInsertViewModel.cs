3a0cd5064482683cd47d5f59e5123ec4ae8ed961
anne nicolle zimmermann <annezdz@hotmail.com>
2020-03-10 13:15:55 -0300

    ErrorMessage Produto Insert View Model

--- DeliveryCORE/Models/Insert/ProdutoInsertViewModel.cs
+++ DeliveryCORE/Models/Insert/ProdutoInsertViewModel.cs
@@ -1,15 +1,24 @@
 ﻿using DTO;
+using DTO.ComplexTypes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Web;
 
 namespace DeliveryCORE.Models.Insert
 {
     public class ProdutoInsertViewModel
     {
+        [DisplayName("Restaurante"), Required(ErrorMessage = "O restaurante deve ser informado")]
+        [StringLength(maximumLength: 30, MinimumLength = 2, ErrorMessage = "O restaurante deve conter entre 2 e 30 caracteres")]
         public ProdutoDTO Restaurante { get; set; }
+        [DisplayName("Nome"), Required(ErrorMessage = "O nome do produto deve ser informado")]
+        [StringLength(maximumLength: 100, MinimumLength = 3, ErrorMessage = "O nome do produto deve conter de 3 e 100 caracteres")]
         public string Nome { get; set; }
+
+        [DisplayName("Valor"), Required(ErrorMessage = "O valor do produto deve ser informado")]
         public double Valor { get; set; }
     }
 }
