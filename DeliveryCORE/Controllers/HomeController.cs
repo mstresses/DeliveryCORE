5d05a46df0337f14b0ab15766c50574279619132
Mayara Stresser <mstresses@hotmail.com>
2020-03-13 15:26:44 -0300

    ;

--- DeliveryCORE/Controllers/HomeController.cs
+++ DeliveryCORE/Controllers/HomeController.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DeliveryCORE.Models;
-using MapIntegration.Models;
 
 namespace DeliveryCORE.Controllers
 {
@@ -26,18 +25,7 @@ namespace DeliveryCORE.Controllers
         }
 
 
-        public IActionResult Index()
-        {
-            LocationLists model = new LocationLists();
-            var locations = new List<Locations>()
-            {
-                new Locations(1, "Bhubaneswar","Bhubaneswar, Odisha", 20.296059, 85.824539),
-                new Locations(2, "Hyderabad","Hyderabad, Telengana", 17.387140, 78.491684),
-                new Locations(3, "Bengaluru","Bengaluru, Karnataka", 12.972442, 77.580643)
-            };
-            model.LocationList = locations;
-            return View(model);
-        }
+        
 
         public IActionResult Privacy()
         {
